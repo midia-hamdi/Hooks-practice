70c02267411dc5ceda80312433ba1fc432eae7b2
midia-hamdi <midiahamdi367@gmail.com>
2023-07-19 18:12:13 +0330

    add count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,9 +5,9 @@ export default function Counter(){
     const [count, setCount] = useState(0);
     return (
         <>
-            <h1>You Clicked 10 Times!</h1>
+            <h1>You Clicked {count} Times!</h1>
             <input type="number" value="1" />
-            <button>Click me</button>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
         </>
     )
 }
