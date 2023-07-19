87f32f2dd4c02415423ce4c5836a4b9e9da29f76
midia-hamdi <midiahamdi367@gmail.com>
2023-07-19 18:39:08 +0330

    add useEffect

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,18 @@
-import react, {useState} from 'react'
+import react, {useState, useEffect} from 'react'
 
 export default function Counter(){
 
     const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
+
+    useEffect(() => {
+        document.title = `You Clicked ${count} Times!`
+    })
     return (
         <>
             <h1>You Clicked {count} Times!</h1>
-            <input type="number" value="1" />
-            <button onClick={() => setCount(count + 1)}>Click me</button>
+            <input type="number" value={step} onChange={(event) => setStep(parseInt(event.target.value))}/>
+            <button onClick={() => setCount(count + step)}>Click me</button>
         </>
     )
 }
